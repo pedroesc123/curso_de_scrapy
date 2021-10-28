226cbf3c8dad964491251d22419c2b64b490e66b
Pedro Espinoza <pedroesc.9905@gmail.com>
2021-10-28 10:56:38 -0500

    Avanzando teoria

---
+++ quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -0,0 +1,32 @@
+import scrapy
+
+# Titulo de la web = //h1/a/text()
+# Citas = //span[@class ="text" and @itemprop = "text"]/text()
+# Top ten tags = //div[contains(@class, 'tags-box')]//a[contains(@class, 'tag')]/text()
+# Next page button = //ul[@class="pager"]//li[@class="next"]/a/@href
+
+class QuotesSpider(scrapy.Spider):
+    name = 'quotes'
+    start_urls = [
+        'https://quotes.toscrape.com/'
+    ]
+    custom_settings = {
+        'FEED_URI': 'quotes.json',
+        'FEED_FORMAT': 'json'
+    }
+
+    def parse(self, response):
+        title = response.xpath('//h1/a/text()').get()
+        quotes = response.xpath('//span[@class ="text" and @itemprop = "text"]/text()').getall()
+        top_ten_tags = response.xpath('//div[contains(@class, "tags-box")]//a[contains(@class, "tag")]/text()').getall()
+
+        yield {
+            'title': title,
+            'quotes': quotes,
+            'top_ten_tags': top_ten_tags
+        }
+
+        next_page_button_link = response.xpath('//ul[@class="pager"]//li[@class="next"]/a/@href').get()
+        if next_page_button_link:
+            yield response.follow(next_page_button_link, callback = self.parse)
+
