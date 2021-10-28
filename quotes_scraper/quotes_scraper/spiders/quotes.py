11b42d8463ab3a6f5696d40e49351618ddad1265
Pedro Espinoza <pedroesc.9905@gmail.com>
2021-10-28 12:24:40 -0500

    Trabajando con argumentos

--- quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -19,7 +19,7 @@ class QuotesSpider(scrapy.Spider):
         if kwargs:
             quotes = kwargs['quotes']
         quotes.extend(response.xpath('//span[@class ="text" and @itemprop = "text"]/text()').getall())
-
+                
         next_page_button_link = response.xpath('//ul[@class="pager"]//li[@class="next"]/a/@href').get()
         if next_page_button_link:
             yield response.follow(next_page_button_link, callback = self.parse_only_quotes, cb_kwargs = {'quotes': quotes})
@@ -32,11 +32,16 @@ class QuotesSpider(scrapy.Spider):
     def parse(self, response):
         title = response.xpath('//h1/a/text()').get()
         quotes = response.xpath('//span[@class ="text" and @itemprop = "text"]/text()').getall()
-        top_ten_tags = response.xpath('//div[contains(@class, "tags-box")]//a[contains(@class, "tag")]/text()').getall()
+        top_tags = response.xpath('//div[contains(@class, "tags-box")]//a[contains(@class, "tag")]/text()').getall()
+
+        top = getattr(self, 'top', None)
+        if top:
+            top = int(top)
+            top_tags = top_tags[:top]
 
         yield {
             'title': title,
-            'top_ten_tags': top_ten_tags
+            'top_tags': top_tags
         }
 
         next_page_button_link = response.xpath('//ul[@class="pager"]//li[@class="next"]/a/@href').get()
